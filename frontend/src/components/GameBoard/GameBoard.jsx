a7589892914a874e5b3308eaa5a2caf6edccee53
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-06-11 20:08:36 -0400

    GameBoard, Game and Score Implemented

---
+++ frontend/src/components/GameBoard/GameBoard.jsx
@@ -0,0 +1,69 @@
+// src/GameBoard.jsx
+
+import React, { useState, useEffect } from 'react';
+import Board from '../../GameLogic';
+import '../../App';
+
+const GameBoard = () => {
+    const [boardInstance] = useState(new Board());
+    const [board, setBoard] = useState(boardInstance.board);
+    const [score, setScore] = useState(boardInstance.score);
+
+    useEffect(() => {
+        boardInstance.initializeBoard();
+        setBoard([...boardInstance.board]);
+    }, [boardInstance]);
+
+    const handleKeyDown = (e) => {
+        switch (e.key) {
+            case 'ArrowUp':
+                boardInstance.moveYAxis(0);
+                break;
+            case 'ArrowDown':
+                boardInstance.moveYAxis(1);
+                break;
+            case 'ArrowLeft':
+                boardInstance.moveXAxis(0);
+                break;
+            case 'ArrowRight':
+                boardInstance.moveXAxis(1);
+                break;
+            default:
+                return;
+        }
+        setBoard([...boardInstance.board]);
+        setScore(boardInstance.score);
+    };
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    //This is for getting the format from css
+    const getTileClass = (value) => {
+        return value === 0 ? 'cell empty' : `cell cell-${value}`;
+    };
+
+    return (
+        <div>
+            <h1>2048</h1>
+            <h2>Score: {score}</h2>
+            <div className="board">
+                {board.map((row, rowIndex) => (
+                    <React.Fragment key={rowIndex}>
+                        {row.map((cell, cellIndex) => (
+                            <div key={cellIndex} className={getTileClass(cell)}>
+                                {cell !== 0 ? cell : ''}
+                            </div>
+                        ))}
+                    </React.Fragment>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default GameBoard;
